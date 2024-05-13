3e1fd4aaf1364e749a7dfb6fb796fe448a4b3f0d
fjrotgerl <fjrotgerl@gmail.com>
2024-05-13 23:22:37 +0200

    ya funca

--- myapp.js
+++ myapp.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/api/:date", (req,res) => {
+    let inputDate = req.params.date;
+    let unix = "";
+    let utc = "";
+    if (inputDate.includes("-")) {
+        unix = Math.floor(new Date(req.params.date).getTime())
+        utc = new Date(inputDate).toUTCString();
+    } else {
+        unix = inputDate;
+        let aux = new Date(0);
+        aux.setUTCMilliseconds(req.params.date);
+        utc = aux.toUTCString();
+    }
+
+    res.send({"unix": unix, "utc": utc})
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
